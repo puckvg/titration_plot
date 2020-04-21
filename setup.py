7d729a00b1c4e4f3b291f6799994bd196010473d
puck <puck.vangerwen@gmail.com>
2020-04-21 16:09:14 +0200

    works now

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
 name = 'titration_plot',
 packages = ['titration_plot'],
-version = '0.1.3',
+version = '0.1.4',
 license = 'gpl-3.0', 
 description = 'Plotting for titration adapted from https://pypi.org/project/titration/',
 author = 'Puck van Gerwen',
 author_email = 'puck.vangerwen@gmail.com',
 url = 'https://github.com/puckvg/titration_plot',
-download_url = 'https://github.com/puckvg/titration_plot/archive/v_013.tar.gz', 
+download_url = 'https://github.com/puckvg/titration_plot/archive/v_014.tar.gz',
 keywords = ['titration', 'pH'],
 install_requires = ['scipy', 'matplotlib'],
 classifiers = [
@@ -18,7 +18,8 @@ classifiers = [
 'Programming Language :: Python :: 3',
 'Programming Language :: Python :: 3.4',
 'Programming Language :: Python :: 3.5',
-'Programming Language :: Python :: 3.6']
+'Programming Language :: Python :: 3.6', 
+'Programming Language :: Python :: 3.7']
 )
 
 
